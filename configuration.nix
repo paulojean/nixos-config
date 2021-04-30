5606d590e67eac09294d1932d8e9e5c29010ca41
Paulo Sousa <pauloj10@gmail.com>
2021-04-30 22:04:49 +0200

    Move video config from bumblebee to nvidia-offload
    Bumblebee is deprecated, see https://nixos.wiki/wiki/Nvidia

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,13 @@
 
 let
   packages = pkgs.callPackage ./packages { pkgs = pkgs; } ;
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
 in
 {
   imports =
@@ -16,11 +23,12 @@ in
   nixpkgs.config.allowUnfree = true;
 
   hardware.cpu.intel.updateMicrocode = true;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.extraConfig = "
-    [General]
-    Enable=Source,Sink,Media,Socket
-  ";
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = { Enable = "Source,Sink,Media,Socket"; };
+    };
+  };
 
   hardware.pulseaudio = {
     enable = true;
@@ -32,9 +40,13 @@ in
     package = pkgs.pulseaudioFull;
   };
 
-  hardware.bumblebee = {
-    enable = true;
-    pmMethod = "bbswitch";
+  hardware.nvidia.prime = {
+    offload.enable = true;
+
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:0:2:0";
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:1:0:0";
   };
 
   # Use the systemd-boot EFI boot loader.
@@ -59,13 +71,15 @@ in
   networking.useDHCP = false;
   networking.interfaces.enp60s0.useDHCP = true;
   networking.interfaces.wlo1.useDHCP = true;
+  networking.enableIPv6 = false;
 
   system.autoUpgrade.enable = true;
   i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
   };
+
+  console.font = "Lat2-Terminus16";
+  console.keyMap = "us";
 
   # Set your time zone.
   time.timeZone = "Europe/Stockholm";
@@ -78,13 +92,14 @@ in
     jq killall acpi tlp
     bash wget neovim emacs curl whois kitty
     harfbuzzFull
-    bash-completion exa lsd bat ack ag fd gnugrep pstree
-    openssl
+    # bash-completion
+    exa lsd bat ack ag fd gnugrep pstree
+    openssl wireshark
     usbutils ipad_charge
     firefox bitwarden vlc tmux
-    xsel xclip xcape htop sqlite
+    keynav xsel xclip xcape htop sqlite
     git gitAndTools.diff-so-fancy
-    docker docker-compose
+    docker-compose
     lsof pciutils zip unzip unrar bind cacert
     fzf file nnn
     libreoffice
@@ -92,7 +107,6 @@ in
 
     python27Packages.pip python37Packages.pip python3 python
 
-    python37Packages.python-language-server
     terraform-lsp
 
     stack visualvm perl shellcheck
@@ -122,22 +136,33 @@ in
     wmctrl
     dunst
 
-    # bspwm
+    # - bspwm
     i3lock-fancy
     sxhkd socat
     xorg.xwininfo
 
-    # nvidia-offload
+    nvidia-offload
 
-    packages.nord-vpn
     packages.sxhkd-statusd
     packages.teiler
+
+    p7zip
+    epsxe
+
+    playonlinux
+
+    # - tmux-fzf
+    bc
   ];
-  nixpkgs.config.permittedInsecurePackages = ["p7zip-16.02"];
+  nixpkgs.config.permittedInsecurePackages = [
+    "p7zip-16.02"
+    "openssl-1.0.2u"
+  ];
 
   programs = {
-    bash.enableCompletion = true;
+    # bash.enableCompletion = true;
     light.enable = true;
+    steam.enable = true;
   };
 
   fonts.fonts = with pkgs; [
@@ -150,6 +175,8 @@ in
   powerManagement.powertop.enable = true;
   services.tlp.enable = true;
 
+  services.usbmuxd.enable = true;
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -161,32 +188,32 @@ in
   services.xserver =  {
     enable = true;
 
-    # displayManager.startx.enable = true;
-
-    displayManager.sddm = {
+    displayManager.autoLogin.enable = false;
+    displayManager.lightdm = {
       enable = true;
-      autoLogin.enable = false;
-      autoLogin.user = "paulo";
     };
 
-    windowManager.bspwm.enable = true;
+    displayManager.sessionCommands = ''
+      xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" 1.0
+    '';
 
-    # windowManager.i3 = {
-    #   enable = true;
-    #   package = pkgs.i3-gaps;
-    #   extraPackages = with pkgs; [
-    #     dmenu #application launcher most people use
-    #     i3lock #default i3 screen locker
-    #     i3blocks #if you are planning on using i3blocks over i3status
-    #     polybar
-    #  ];
-    # };
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+        polybar
+     ];
+    };
 
     layout = "us";
     xkbVariant = "intl";
     xkbOptions = "eurosign:e, caps:ctrl_modifier";
 
-    videoDrivers = [ "modesetting" ];
+    # "displaylink" to make nix recognize HDMI entry
+    videoDrivers = [ "displaylink" "modesetting" ];
   };
 
   # Enable touchpad support.
@@ -209,10 +236,14 @@ in
     inactiveOpacity = 0.9;
     opacityRules = [
       "100:class_g = 'Rofi'"
+      "100:class_g = 'Firefox'"
     ];
   };
 
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    docker.enable = true;
+    podman.enable = true;
+  };
 
   security.pam.services.gdm.enableGnomeKeyring = true;
   security.sudo.extraRules = [
@@ -220,6 +251,10 @@ in
       commands = [
         {
           command = "${pkgs.light}/bin/light";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "${pkgs.tlp}/bin/bluetooth";
           options = [ "NOPASSWD" ];
         }
       ];
@@ -232,7 +267,7 @@ in
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.paulo = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "video" "audio" "networkmanager" "kvm" "render" "sddm" "light" ];
+    extraGroups = [ "wheel" "docker" "video" "audio" "networkmanager" "kvm" "render" "sddm" "lightdm" "light" "usbmux" ];
   };
 
   nixpkgs.overlays = [ (self: super: {
@@ -242,9 +277,10 @@ in
     polybar = super.polybar.override {
       pulseSupport = true;
       mpdSupport = true;
-    };
-    dunst = super.dunst.override {
-      dunstify = true;
+      # i3Support = true;
+      i3GapsSupport = true;
+      jsoncpp = super.jsoncpp;
+      i3 = super.i3;
     };
   })];
 
@@ -252,6 +288,6 @@ in
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 
 }
